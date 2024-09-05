76d3d88e2c5c9bb487edf58340badf262427d855
Bruna Savelli <bnsavelli@gmail.com>
2024-09-05 09:52:43 -0300

    feat: create get by id method

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,7 +5,7 @@ import doceRoutes from "./doces.routes.js";
 const routes = Router();
 
 routes.get("/", (req, res) => {
-    return res.status(200).send({message: "Servidor funfando 🥵"})
+    return res.status(200).send({ message: "Servidor funfando 🥵" })
 });
 
 routes.use("/doces", doceRoutes);
