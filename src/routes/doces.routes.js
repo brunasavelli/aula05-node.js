dc1b634cf032f997589d3b9d39bc632506d3bb81
Bruna Savelli <bnsavelli@gmail.com>
2024-09-05 11:44:34 -0300

    feat: create delete method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const docesRoutes = Router();
 
-const guloseimas = [
+let guloseimas = [
     {
         id: 1,
         nome: "Trufa",
@@ -80,6 +80,21 @@ docesRoutes.put("/:id", (req, res) => {
         message:"guloseima atualizada",
         guloseima,
     });
+});
+
+//Rota para deletar uma guloseima.
+docesRoutes.delete("/:id", (req, res) => {
+    const{ id } = req.params
+
+    const guloseima = guloseimas.find((doces) => doces.id === Number(id));
+
+    if (!guloseima) {
+        return res.status(404).send({message: "Guloseima não encontrada"})
+    };
+
+    guloseimas = guloseimas.filter((doces) => doces.id !== Number(id));
+
+    return res.status(200).send({message: "Guloseima deletada!"})
 });
 
 export default docesRoutes;
